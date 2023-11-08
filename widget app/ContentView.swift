ef6dd171b1ef6a083dc683c19ea21bf66aa52505
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-11-08 09:24:46 +0100

    Initial Commit

---
+++ widget app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  widget app
+//
+//  Created by Matteo Perotta on 08/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ widget app/widget_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  widget_appApp.swift
+//  widget app
+//
+//  Created by Matteo Perotta on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct widget_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
